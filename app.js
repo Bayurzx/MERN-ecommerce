d9044435eeb78a8a7b06aedc1c52778fd1a46221
Bayurzx <bayurzx@gmail.com>
2020-11-07 16:55:26 +0100

    First commit => I added the routes, DB(schema, model), middlewares

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+
+const app = express();
+dotenv.config()
+const port = process.env.PORT || 1337;
+const db = process.env.MONGO_LOCALHOST;
+const userRoutes = require('./routes/user');
+
+// DB
+mongoose.connect(db, {
+  useNewUrlParser : true,
+  useUnifiedTopology: true,
+  useCreateIndex : true
+  })
+  .then(() => console.log("Database connection successful"))
+
+// middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// Routes tunred into middleware!
+app.use('/api', userRoutes);
+
+app.listen(port, () => {
+  console.log("The server running on port", port);
+})
